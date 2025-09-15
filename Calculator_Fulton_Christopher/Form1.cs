f8199c0519af3b549a4a958d8332a56b71b4bf8d
Yung-Modi <chrisreaper404@gmail.com>
2025-09-15 12:33:03 -0500

    Initial commit

---
+++ Calculator_Fulton_Christopher/Form1.cs
@@ -0,0 +1,10 @@
+namespace Calculator_Fulton_Christopher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Calculator_Fulton_Christopher
+
